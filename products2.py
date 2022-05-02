da9d585457d98f88577602ba0b5d8bd69e92c240
PaulSurgeon <wonderfulhumble@gmail.com>
2022-05-03 06:22:12 +0800

    continue

--- products2.py
+++ products2.py
@@ -5,6 +5,7 @@ with open("products.csv", "r") as f:
         s = line.strip().split(",")
         print(s)
 
+#另一種寫法
 products2 = []
 with open("products.csv", "r") as f:
     for line in f:
@@ -12,3 +13,14 @@ with open("products.csv", "r") as f:
         products2.append([name, price])
 
 print(products2)
+
+# 去除標題列，使用continue功能。
+products3 = []
+with open("products.csv", "r") as f:
+    for line in f:
+        if "商品,價格" in line:
+            continue
+        name, price = line.strip().split(",")
+        products3.append([name, price])
+
+print(products3)
